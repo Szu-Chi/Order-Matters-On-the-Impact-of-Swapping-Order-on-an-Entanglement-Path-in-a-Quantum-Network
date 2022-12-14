51f25d52eb10669007e3adf36230b4f0f68b64cc
Szu-Chi <r310102930719@gmail.com>
2022-12-15 03:30:17 +0800

    Experimental Result

--- swapping_order_simulation.py
+++ swapping_order_simulation.py
@@ -93,12 +93,39 @@ class Graph:
                 mv = MetricVector(x.w, x.W, p)
         return mv
 
+    def attempt_en_by_name(self, x, y):
+        x = self.graph[x]
+        y = self.graph[y] 
+        if type(x.p) == dict:
+            assert y.name in x.p.keys()
+            p=x.p[y.name]
+        else:
+            p=x.p
+        
+        if type(x.w) == dict:
+            if y.name in x.w.keys():
+                attempt = np.random.random(x.w[y.name])
+            else:
+                return 0
+        else:
+                attempt = np.random.random(x.w)
+        success = np.where(attempt<p)[0].size
+        return success
+
     def calc_mv_with_enswap(self, x, y ,z):
         # connecting nodes x and z, after performing entanglement swapping at node y
         mv_xy = self.calc_mv_by_name(x,y)
         mv_yz = self.calc_mv_by_name(y,z)
         mv_xz = mv_xy.enswap(mv_yz,self.graph[y].q)
         return mv_xz
+
+    def attempt_enswap(self, x, y ,z):
+        # connecting nodes x and z, after performing entanglement swapping at node y
+        en_xy = self.attempt_en_by_name(x,y)
+        en_yz = self.attempt_en_by_name(y,z)
+        attempt = np.random.random(min(en_xy, en_yz))
+        success = np.where(attempt<self.graph[y].q)[0].size
+        return success
 
     def calc_mv_with_order(self, order):
         g = copy.deepcopy(self.graph)
@@ -138,8 +165,62 @@ class Graph:
             else:
                 result_key += f'_{c}'
         return linked[result_key]
-    
-#%%
+
+    def attempt_en_with_order(self, order):
+        g = copy.deepcopy(self.graph)
+        linked = {}
+        for o in order:
+            l = list(g[o].w.keys())
+            l0_linked = f'{o}_{l[0]}' in linked.keys() or f'{l[0]}_{o}' in linked.keys()
+            l1_linked = f'{o}_{l[1]}' in linked.keys() or f'{l[1]}_{o}' in linked.keys()
+            if l0_linked==False and l1_linked==False:
+                success = self.attempt_enswap(l[0], o, l[1])
+                
+            elif l0_linked==False and l1_linked==True:
+                s = self.attempt_en_by_name(o, l[0])
+                s_link = linked[f'{o}_{l[1]}']
+                attempt = np.random.random(min(s_link, s))
+                success = np.where(attempt<g[o].q)[0].size
+            elif l0_linked==True and l1_linked==False:
+                s = self.attempt_en_by_name(o, l[1])
+                s_link = linked[f'{o}_{l[0]}']
+                attempt = np.random.random(min(s_link, s))
+                success = np.where(attempt<g[o].q)[0].size
+            elif l0_linked==True and l1_linked==True:
+                s_link0 = linked[f'{o}_{l[0]}']
+                s_link1 = linked[f'{o}_{l[1]}']
+                attempt = np.random.random(min(s_link0, s_link1))
+                success = np.where(attempt<g[o].q)[0].size
+            
+            linked[f'{l[0]}_{l[1]}'] = success
+            linked[f'{l[1]}_{l[0]}'] = success
+            g[l[0]].w[l[1]]=-1
+            g[l[1]].w[l[0]]=-1
+            del g[l[0]].w[o]
+            del g[l[1]].w[o]
+            del g[o]
+        result_key = ""
+        for c in list(g.keys()):
+            if result_key=="":
+                result_key = c
+            else:
+                result_key += f'_{c}'
+        return linked[result_key]
+
+
+def simulate_swapping_effect(graph, order, testing_tiems=100000):
+    mv = graph.calc_mv_with_order(order)
+    print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}', end='  ')
+    en_attem = []
+    for _ in range(testing_tiems):
+        en_attem.append(graph.attempt_en_with_order(order))
+    un, co = np.unique(en_attem, return_counts=True)
+    f = np.zeros(mv.P.shape[0])
+    for i,u in enumerate(un):
+        f[u] = co[i]
+    print(f'number of times: {f}',f'Throughput: {sum(en_attem)}')
+
+
 print('\n')
 print('   1     2     3     3   ')
 print('s-----x-----y-----z-----t')
@@ -156,36 +237,21 @@ graph.add_vertex(y)
 graph.add_vertex(z)
 graph.add_vertex(t)
 
+#%%
 order = ['x', 'y', 'z']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order)
 
 order = ['x', 'z', 'y']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order)
 
 order = ['y', 'x', 'z']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order)
 
 order = ['y', 'z', 'x']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order)
 
 order = ['z', 'y', 'x']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
-
-#%%
-#%%
-ext = [0]
-for order in list(itertools.permutations(['x', 'y', 'z'])):
-    mv = graph.calc_mv_with_order(order)
-    if mv.ext() > max(ext):
-        opt_order = order 
-    ext.append(mv.ext())
-
-print(f'opimize_order: {opt_order}', f'EXT: {round(max(ext),3)}')
+simulate_swapping_effect(graph, order)
 
 
 #%%
@@ -214,18 +280,14 @@ graph.add_vertex(g)
 graph.add_vertex(t)
 
 order = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order, testing_tiems=100000)
 
 
 order = ['a', 'c', 'e', 'g', 'b', 'f', 'd']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
-
+simulate_swapping_effect(graph, order, testing_tiems=100000)
 
 order = ['c', 'b', 'e', 'f', 'd', 'g', 'a']
-mv = graph.calc_mv_with_order(order)
-print(f'metric vector: {np.round(mv.P, 3)}', f'EXT: {np.round(mv.ext(), 3)}')
+simulate_swapping_effect(graph, order, testing_tiems=100000)
 
 #%%
 ext = [0]
